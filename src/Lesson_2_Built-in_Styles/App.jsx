4d119e9a491de3e23e2092f38853a6d536a010e4
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-12 10:23:47 +0200

    1

---
+++ src/Lesson_2_Built-in_Styles/App.jsx
@@ -0,0 +1,50 @@
+// import "./App.css";
+import Card from "./components/Card";
+import users from "../data.json";
+
+function App() {
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <Card
+            key={user.id}
+            name={user.name}
+            // address={user.address}
+            email={user.email}
+            phone={user.phone}
+          />
+        );
+      })}
+      <Card
+        key={777}
+        name="Serhii"
+        phone={777}
+        email="qwerty@qwerty.com"
+        // address="motel"
+      />
+    </div>
+  );
+}
+
+export default App;
+
+// function App() {
+//   console.log("users: >>", users);
+//   return (
+//     <div>
+//       <Card
+//         name="Alex"
+//         phone={555555555}
+//         email="qwery@gmail.com"
+//         qwery="qwery"
+//       />
+//       <Card
+//         name="Serhii"
+//         phone={111111111}
+//         email="qwertyqwerty@gmail.com"
+//         qwery="qwertyqwerty"
+//       />
+//     </div>
+//   );
+// }
