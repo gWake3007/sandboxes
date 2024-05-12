c15595672b4812e69d9ab1a23c77aa99d9cad874
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-12 20:41:21 +0200

    1

--- src/Lesson_2_Built-in_Styles/App.jsx
+++ src/Lesson_2_Built-in_Styles/App.jsx
@@ -5,23 +5,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   console.log("users: >>", users);
-//   return (
-//     <div>
-//       <Card
-//         name="Alex"
-//         phone={555555555}
-//         email="qwery@gmail.com"
-//         qwery="qwery"
-//       />
-//       <Card
-//         name="Serhii"
-//         phone={111111111}
-//         email="qwertyqwerty@gmail.com"
-//         qwery="qwertyqwerty"
-//       />
-//     </div>
-//   );
-// }
