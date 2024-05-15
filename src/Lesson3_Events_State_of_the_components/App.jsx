59e1e614621b0fec110034f09ead88ff365a243e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-15 20:39:42 +0200

    1

--- src/Lesson3_Events_State_of_the_components/App.jsx
+++ src/Lesson3_Events_State_of_the_components/App.jsx
@@ -1,7 +1,15 @@
-import Card_List from "./components/Card_List/Card_List";
+// import Card_List from "./components/Card_List/Card_List";
 
 function App() {
-  return <Card_List />;
+  const handleClick = (id) => {
+    console.log(id);
+  };
+  return (
+    <div>
+      <button onClick={() => handleClick(5)}>qwerty</button>
+      <button onClick={() => handleClick(50)}>qwerty</button>
+    </div>
+  );
 }
 
 export default App;
