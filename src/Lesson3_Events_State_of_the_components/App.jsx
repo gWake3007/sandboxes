838aa55482456ecec5a3999a5a9052dccd139f11
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-19 23:12:38 +0200

    1

--- src/Lesson3_Events_State_of_the_components/App.jsx
+++ src/Lesson3_Events_State_of_the_components/App.jsx
@@ -1,5 +1,43 @@
 // import Card_List from "./components/Card_List/Card_List";
 
+//? useState - хук в реакті.
+// import { useState } from "react";
+
+// const App = () => {
+//   // let clicks = 0;
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     // clicks = clicks + 1;
+//     setClicks(clicks + 1);
+//   };
+
+//   return <button onClick={handleClick}>Current: {clicks}</button>;
+// };
+
+//?Приклад з двума змінними.
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Current: {clicks}</button>
+//       <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
+//       {isOpen && <p>Now you can see me!</p>}
+//     </>
+//   );
+// };
+
 function App() {
   const handleClick = (id) => {
     console.log(id);
