8c0d43750e12b5226553893baeafa762bfc9f8ff
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-22 12:35:09 +0200

    1

--- src/Lesson4_Life_Cycle_of_a_component/App.jsx
+++ src/Lesson4_Life_Cycle_of_a_component/App.jsx
@@ -1,17 +1,32 @@
 import { useState, useEffect } from "react";
 // import Timer from "./components/Timer/Timer";
 
+const createInitrialCounter = (key) => {
+  const localCounter = localStorage.getItem(key);
+  // if (localCounter) {
+  //   return JSON.parse(localCounter);
+  // }
+  // return 0;
+  return localCounter ? JSON.parse(localCounter) : 0;
+};
 function App() {
   // const [isShowTimer, setIsShowTimer] = useState(false);
   //?Відслідковування ефектів button.
-  const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
+  const [counter, setCounter] = useState(() =>
+    createInitrialCounter("counter")
+  );
+  //?Приклад відслідковування localStorage для запису його та повернення значення якщо воно є і якщо його немає то дефолтне 0
+  const [counter2, setCounter2] = useState(() =>
+    createInitrialCounter("counter2")
+  );
   const total = counter + counter2;
   useEffect(() => {
+    localStorage.setItem("counter", counter);
     console.log("counter", counter);
     console.log("total", total);
   }, [counter, total]);
   useEffect(() => {
+    localStorage.setItem("counter2", counter2);
     console.log("counter2", counter2);
     console.log("total", total);
   }, [counter2, total]);
