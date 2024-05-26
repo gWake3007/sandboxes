1292e473a65c1745e1bbe3f5b9f1d4675f83559f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-26 16:35:28 +0200

    1

--- src/Lesson5_Forms/App.jsx
+++ src/Lesson5_Forms/App.jsx
@@ -1,4 +1,17 @@
 // import { useState, useEffect } from "react";
+import LoginForm from "./components/LoginForm/LoginForm";
+import MyComponent from "./components/MyComponent/MyComponent";
 
-function App() {}
+function App() {
+  const handleLogin = (userData) => {
+    console.log(userData);
+  };
+  return (
+    <>
+      <h1>Please login to your account!</h1>
+      <LoginForm onLogin={handleLogin} />
+      <MyComponent />
+    </>
+  );
+}
 export default App;
