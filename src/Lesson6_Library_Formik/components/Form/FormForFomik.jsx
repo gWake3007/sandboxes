208aeb7a0d937e68b50af9de64c113998122ae3f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-05-31 23:24:32 +0200

    1

--- src/Lesson6_Library_Formik/components/Form/FormForFomik.jsx
+++ src/Lesson6_Library_Formik/components/Form/FormForFomik.jsx
@@ -1,6 +1,7 @@
 import css from "./FormForFomik.module.css";
+import * as Yup from "yup";
 import { useId } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const initialValues = {
   username: "",
@@ -8,6 +9,19 @@ const initialValues = {
   message: "",
   level: "good",
 };
+
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Must be a valid email!").required("Required"),
+  message: Yup.string()
+    .min(3, "Too Short!")
+    .max(256, "Too Long!")
+    .required("Required"),
+  level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+});
 
 const FormForFomik = () => {
   const nameFieldId = useId();
@@ -19,36 +33,62 @@ const FormForFomik = () => {
     actions.resetForm();
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form className={css.form}>
-        <label htmlFor={nameFieldId}>UserName</label>
-        <Field
-          className={css.field}
-          type="text"
-          name="username"
-          id={nameFieldId}
-        />
-        <label htmlFor={emailFieldId}>Email</label>
-        <Field
-          className={css.field}
-          type="email"
-          name="email"
-          id={emailFieldId}
-        />
-        <label htmlFor={msgFieldId}>Message</label>
-        <Field
-          className={css.field}
-          as="textarea"
-          cols="20"
-          rows="5"
-          name="message"
-          id={msgFieldId}
-        />
-        <Field className={css.field} as="select" name="level" id={levelFieldId}>
-          <option value="o1">Option 1</option>
-          <option value="o2">Option 2</option>
-          <option value="o3">Option 3</option>
-        </Field>
+        <div className={css.container}>
+          <label htmlFor={nameFieldId}>UserName</label>
+          <Field
+            className={css.field}
+            type="text"
+            name="username"
+            id={nameFieldId}
+          />
+          <ErrorMessage
+            className={css.error}
+            name="username"
+            component="span"
+          />
+        </div>
+        <div className={css.container}>
+          <label htmlFor={emailFieldId}>Email</label>
+          <Field
+            className={css.field}
+            type="email"
+            name="email"
+            id={emailFieldId}
+          />
+          <ErrorMessage className={css.error} name="email" component="span" />
+        </div>
+        <div className={css.container}>
+          <label htmlFor={msgFieldId}>Message</label>
+          <Field
+            className={css.field}
+            as="textarea"
+            cols="20"
+            rows="5"
+            name="message"
+            id={msgFieldId}
+          />
+          <ErrorMessage className={css.error} name="message" component="span" />
+        </div>
+        <div className={css.container}>
+          <label htmlFor={levelFieldId}>Service satisfaction level</label>
+          <Field
+            className={css.field}
+            as="select"
+            name="level"
+            id={levelFieldId}
+          >
+            <option value="good">Option 1</option>
+            <option value="neutral">Option 2</option>
+            <option value="bad">Option 3</option>
+          </Field>
+          <ErrorMessage className={css.error} name="level" component="span" />
+        </div>
         <button className={css.btn} type="submit">
           Submit
         </button>
