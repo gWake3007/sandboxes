1e9b57ade497e381a5592ab9006cbc7a4ac70c57
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-01 12:08:37 +0200

    1

--- src/Lesson6_Library_Formik/App.jsx
+++ src/Lesson6_Library_Formik/App.jsx
@@ -1,9 +1,11 @@
 import FormForFomik from "./components/Form/FormForFomik";
+import Formik_Library_Mentor_Example from "./components/Formik_Library_Mentor_Example/Formik_Library_Mentor_Example";
 
 function App() {
   return (
     <>
       <FormForFomik />
+      <Formik_Library_Mentor_Example />
     </>
   );
 }
---
+++ src/Lesson6_Library_Formik/components/Formik_Library_Mentor_Example/Formik_Library_Mentor_Example.jsx
@@ -0,0 +1,8 @@
+import css from "./Formik_Library_Mentor_Example.module.css";
+import { Formik } from "formik";
+
+const Formik_Library_Mentor_Example = () => {
+  return <div>Formik_Library_Mentor_Example</div>;
+};
+
+export default Formik_Library_Mentor_Example;
