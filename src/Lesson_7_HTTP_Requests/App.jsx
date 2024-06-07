df11f0e9f2ed8a9976d68f29d01bd9696bbe808b
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-07 22:55:52 +0200

    1

--- src/Lesson_7_HTTP_Requests/App.jsx
+++ src/Lesson_7_HTTP_Requests/App.jsx
@@ -1,15 +1,34 @@
-import FormForFomik from "./components/Form/FormForFomik";
-import Formik_Library_Mentor_Example from "./components/Formik_Library_Mentor_Example/Formik_Library_Mentor_Example";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ArticleList from "./components/ArticleList/ArticleList";
 
 function App() {
-  const submit = (data) => {
-    console.log(data);
-  };
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchArticles();
+  }, []);
   return (
     <>
-      <FormForFomik />
-      <Formik_Library_Mentor_Example submit={submit} />
+      <h1>Latest Articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </>
   );
 }
 export default App;
+
+//?Стан loading потрібен для того щоб при очікуванні коли підтягується API в try, catch, finally змінювати стан та спочатку додавати Loading і по завершенню прибирати!
