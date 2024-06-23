8b39a0ffa98bf911abc57b8643d811d138993e2e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-23 20:48:28 +0200

    1

--- src/Lesson_11_Redux/pages/HomePage.jsx
+++ src/Lesson_11_Redux/pages/HomePage.jsx
@@ -1,14 +1,23 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { actionDeposit } from "../reduxStore/store";
 
 const HomePage = () => {
   const balance = useSelector((state) => {
     return state.balance.value;
   });
+  const dispath = useDispatch();
+
+  const handleDeposit = () => {
+    dispath(actionDeposit(100));
+  };
   return (
     <div>
       HomePage
       <hr />
       Balance: {balance}
+      <hr />
+      <button onClick={handleDeposit}>Deposit</button>
+      <button>Withdraw</button>
     </div>
   );
 };
--- src/Lesson_11_Redux/reduxStore/store.js
+++ src/Lesson_11_Redux/reduxStore/store.js
@@ -4,11 +4,30 @@ import { createStore } from "redux";
 const initialState = {
   balance: {
     value: 1000,
+    comments: "",
   },
+  users: {},
+  items: {},
 };
 
+export const actionDeposit = (value) => ({
+  type: "balance/deposit",
+  payload: value,
+});
+
 const reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "balance/deposit":
+      return {
+        ...state,
+        balance: {
+          ...state.balance,
+          value: action.payload,
+        },
+      };
+    default:
+      return state;
+  }
 };
 
 export const store = createStore(reducer, composeWithDevTools());
@@ -16,3 +35,6 @@ export const store = createStore(reducer, composeWithDevTools());
 //?initialState - Це стан для ВСЬОГО нашого проєкту! ВАЖЛИВО!!!
 //?Перший раз в state(при рендері) завжди прилітає undefined. Тому ми вказали state=initialState - щоб якщо прилітає undefined то прилітав пустий об'єкт значенть.
 //?composeWithDevTools() - Потрібен для того щоб в інструментах розробника була вкладка Redux з якою можна працювати. По типу як до цього було завантаженний додаток щоб бачити components та profiler.
+//?actionDeposit - type(що ми хочемо змінити),payload(на що ми хочемо змінити)
+//?Обов'язково в reducer де пишемо через switch case повертати новий об'єкт де значення буде action.payload!ВАЖЛИВО!!! По дефолту повертати той state що в нас є. Якщо прийде якийсь не той type.
+//?За допомогою спред оператору ... в функції switch case щоб не втрачати значення ми підтягуємо їх.
