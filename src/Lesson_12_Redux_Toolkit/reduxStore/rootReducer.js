f965b5f2e5143c98616cfe94d54a1d9fb87e3595
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-06-27 23:06:18 +0200

    1

--- src/Lesson_12_Redux_Toolkit/reduxStore/rootReducer.js
+++ src/Lesson_12_Redux_Toolkit/reduxStore/rootReducer.js
@@ -1,9 +1,14 @@
 // import { balanceReducer } from "./balanceSlice_Old";
 import { combineReducers } from "redux";
 import { balanceReducer } from "./balanceSlice";
-import { itemsReducer } from "./balanceSlice";
+import { itemsReducer } from "./itemsSlice";
 
-export const reducer = {
+export const rootReducer = combineReducers({
+  // balance: balanceReducer,
   balance: balanceReducer,
   items: itemsReducer,
-};
+});
+
+//?Імпортуємо balanceReducer тільки з файлу де зробленно новим методом createSlice!!!ВАЖЛИВО!!!
+
+//?З використанням persistReducer огортаємо наш root в combineReducers. ВАЖЛИВО!!!
